87f4a39a1af631a38a8529e90f49179a096dccb2
Elyas Hamedeh <hamedahty2000@gmail.com>
2025-05-12 18:47:03 +0300

    corrected copoun into couopn in routes names

--- app/requests/requestReview/page.tsx
+++ app/requests/requestReview/page.tsx
@@ -1,5 +1,4 @@
 import { RequestReviewForm } from "@/components/RequestReviewForm";
-import { Card } from "@/components/ui/card";
 export default function RequestReview() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center  p-6 md:p-10">
