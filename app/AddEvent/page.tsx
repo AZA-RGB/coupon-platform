56462cf66b4cb00450be2cd09ffb60d3d54c7da2
Elyas Hamedeh <hamedahty2000@gmail.com>
2025-05-17 13:19:05 +0300

    ge

--- app/AddEvent/page.tsx
+++ app/AddEvent/page.tsx
@@ -26,12 +26,12 @@ const formSchema = z.object({
   event_description: z.string().min(30).max(200),
   event_expiration_date: z.coerce.date(),
   coupon_per_provider: z.coerce.number().min(1),
-  images: z.array(z.instanceof(File)).min(1,'min imagwe'),
+  images: z.array(z.instanceof(File)).min(1, "min imagwe"),
 });
 
 export default function AddEvent() {
-  const t = useTranslations('addEvent');
-  
+  const t = useTranslations("addEvent");
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -49,129 +49,120 @@ export default function AddEvent() {
       );
     } catch (error) {
       console.error("Form submission error", error);
-      toast.error(t('validation.formError'));
+      toast.error(t("validation.formError"));
     }
   }
 
   return (
-    <div className="flex flex-col items-center max-h-svh overflow-clip justify-center md:p-10 pt-10 p-2">
-      <div className="w-full md:max-w-2/3">
-        <div>
-          <Card className="p-0">
-            <CardContent className="grid grid-col-1 lg:grid-cols-2 p-0">
-              <Form {...form}>
-                <form
-                  onSubmit={form.handleSubmit(onSubmit)}
-                  className="space-y-10 p-10 w-full max-w-3xl mx-auto my-auto max-h-[calc(100dvh-5rem)] overflow-scroll"
-                >
-                  <FormField
-                    control={form.control}
-                    name="images"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>{t('eventCoverImages')}</FormLabel>
-                        <FormControl>
-                          <FileUploadDropzone field={field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
+    
+    <>
+    <title>Add Event</title>
+    <div className="flex  flex-col   w-full ">
+      <Card className="max-w-xl self-center  p-0">
+        <CardContent >
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="space-y-10 p-10 w-full mx-auto my-auto "
+            >
+              <FormField
+                control={form.control}
+                name="images"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("eventCoverImages")}</FormLabel>
+                    <FormControl>
+                      <FileUploadDropzone field={field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-                  <FormField
-                    control={form.control}
-                    name="event_title"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>{t('eventTitle')}</FormLabel>
-                        <FormControl>
-                          <Input
-                            placeholder={t('eventTitlePlaceholder')}
-                            type="text"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
+              <FormField
+                control={form.control}
+                name="event_title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("eventTitle")}</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder={t("eventTitlePlaceholder")}
+                        type="text"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-                  <FormField
-                    control={form.control}
-                    name="event_description"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>{t('eventDescription')}</FormLabel>
-                        <FormControl>
-                          <Textarea placeholder="" {...field} />
-                        </FormControl>
-                        <FormDescription>
-                          {t('addDescription')}
-                        </FormDescription>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
+              <FormField
+                control={form.control}
+                name="event_description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("eventDescription")}</FormLabel>
+                    <FormControl>
+                      <Textarea placeholder="" {...field} />
+                    </FormControl>
+                    <FormDescription>{t("addDescription")}</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-                  <FormField
-                    control={form.control}
-                    name="event_expiration_date"
-                    render={({ field }) => (
-                      <FormItem className="flex flex-col">
-                        <FormLabel>{t('expirationDate')}</FormLabel>
-                        <DatetimePicker
-                          {...field}
-                          format={[
-                            ["months", "days", "years"],
-                            ["hours", "minutes", "am/pm"],
-                          ]}
-                        />
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
+              <FormField
+                control={form.control}
+                name="event_expiration_date"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel>{t("expirationDate")}</FormLabel>
+                    <DatetimePicker
+                      {...field}
+                      format={[
+                        ["months", "days", "years"],
+                        ["hours", "minutes", "am/pm"],
+                      ]}
+                    />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-                  <FormField
-                    control={form.control}
-                    name="coupon_per_provider"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>{t('maxPerProvider')}</FormLabel>
-                        <FormControl>
-                          <Input 
-                            placeholder={t('maxPerProviderPlaceholder')} 
-                            type="number" 
-                            {...field} 
-                          />
-                        </FormControl>
-                        <FormDescription>
-                          {t('maxPerProviderDescription')}
-                        </FormDescription>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
+              <FormField
+                control={form.control}
+                name="coupon_per_provider"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("maxPerProvider")}</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder={t("maxPerProviderPlaceholder")}
+                        type="number"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      {t("maxPerProviderDescription")}
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-                  <div className="flex flex-1 place-content-around">
-                    <Button type="submit">{t('submit')}</Button>
-                    <Link href="/dashboard">
-                      <Button variant="outline">{t('cancel')}</Button>
-                    </Link>
-                  </div>
-                </form>
-              </Form>
-
-              <div className="bg-primary relative">
-                <img
-                  src="../addCoupon.jpeg"
-                  alt="imageAlt"
-                  className="absolute h-full object-cover brightness-[1.1]"
-                />
+              <div className="flex flex-1 place-content-around">
+                <Button type="submit">{t("submit")}</Button>
+                <Link href="/dashboard">
+                  <Button variant="outline">{t("cancel")}</Button>
+                </Link>
               </div>
-            </CardContent>
-          </Card>
-        </div>
-      </div>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
     </div>
+    </>
+
   );
 }
