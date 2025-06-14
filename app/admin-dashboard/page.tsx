626096302b6f54c15e5f68e87a1de9590c9e36b0
Elyas Hamedeh <hamedahty2000@gmail.com>
2025-06-15 02:29:18 +0300

    admin dashboard and dashboard with translation and responsivness

---
+++ app/admin-dashboard/page.tsx
@@ -0,0 +1,234 @@
+import { CardsCarousel } from "@/components/dashboard/cards-carousel";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { requestsData, topCategoriesData } from "../providers/constants";
+import {
+  DollarSign,
+  LineChart,
+  ShoppingCart,
+  Users,
+  Package,
+} from "lucide-react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Link from "next/link";
+import TopCustomersList from "@/components/admin-dashboard/TopCustomersList";
+import SalesOverviewChart from "@/components/admin-dashboard/SalesOverviewChart";
+import { ProviderChart } from "@/components/admin-dashboard/ProvidersChart";
+import TopCouponsTable from "@/components/dashboard/top-coupons-table";
+import CouponsTable from "@/components/admin-dashboard/CouponsTable";
+import { useLocale, useTranslations } from "next-intl";
+
+const AdminDashboardPage = () => {
+  const t = useTranslations();
+  const cardData = [
+    {
+      title: t("AdminDashboard.totalSales"),
+      value: "867,878",
+      icon: <ShoppingCart className="h-5 w-5" />,
+    },
+    {
+      title: t("AdminDashboard.newUsers"),
+      value: "12,345",
+      icon: <Users className="h-5 w-5" />,
+    },
+    {
+      title: t("AdminDashboard.orders"),
+      value: "5,678",
+      icon: <Package className="h-5 w-5" />,
+    },
+    {
+      title: t("AdminDashboard.revenue"),
+      value: "$89,012",
+      icon: <DollarSign className="h-5 w-5" />,
+    },
+    {
+      title: t("AdminDashboard.visitors"),
+      value: "234,567",
+      icon: <LineChart className="h-5 w-5" />,
+    },
+  ];
+
+  return (
+    <div className="p-4 sm:p-2 lg:p-4 space-y-6">
+      {/* Top statistics cards */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4">
+        {cardData.map((card, index) => (
+          <Card
+            key={index}
+            className="flex flex-row justify-between items-center p-4"
+          >
+            <div>
+              <div className="text-muted-foreground text-sm">{card.title}</div>
+              <div className="text-lg font-semibold">{card.value}</div>
+            </div>
+            <div className="bg-primary text-white rounded-lg p-3">
+              {card.icon}
+            </div>
+          </Card>
+        ))}
+      </div>
+
+      {/* Main cards */}
+      <div className=" grid grid-cols-1 lg:grid-cols-3 gap-4">
+        <TopCategoriesCard />
+
+        <div className="w-full h-[35vh] ">
+          <CardsCarousel />
+        </div>
+
+        <div className="w-full h-[35vh] ">
+          <RequestsCard requestsData={requestsData} />
+        </div>
+      </div>
+
+      {/* Sales Overview Chart */}
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <Card className="lg:col-span-1 md:p-4">
+          <CardHeader>
+            <CardTitle className="text-lg">
+              {t("AdminDashboard.salesOverview")}
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="p-0">
+            <SalesOverviewChart />
+          </CardContent>
+        </Card>
+        <Card className="lg:col-span-1  rounded-lg">
+          <CardTitle className="text-lg px-4">
+            {t("AdminDashboard.providers")}
+          </CardTitle>
+          <CardContent>
+            <ProviderChart />
+          </CardContent>
+        </Card>
+      </div>
+
+      {/* Tables */}
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-3">
+        <div className="lg:col-span-3">
+          <CouponsTable />
+        </div>
+        <div className=" lg:col-span-1">
+          <TopCustomersList />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const TopCategoriesCard = () => {
+  const t = useTranslations();
+  return (
+    <Card className="h-[35vh] p-4 gap-2">
+      <CardTitle className="text-primary text-lg m-0">
+        {t("Types.topCategories")}
+      </CardTitle>
+      <div className="overflow-auto ">
+        <Table className="min-w-full text-sm">
+          <TableHeader>
+            <TableRow>
+              <TableHead className="py-2 px-4 text-start">
+                {t("Types.rank")}
+              </TableHead>
+              <TableHead className="py-2 px-4 text-start">
+                {t("Types.category")}
+              </TableHead>
+              <TableHead className="py-2 px-4 text-start">
+                {t("Types.sales")}
+              </TableHead>
+              <TableHead className="py-2 px-4 text-start">
+                {t("Types.popularity")}
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {topCategoriesData.slice(0, 5).map((row, index) => (
+              <TableRow key={index} className="hover:bg-secondary">
+                <TableCell className="py-2 px-4">{row.rank}</TableCell>
+                <TableCell className="py-2 px-4">{row.category}</TableCell>
+                <TableCell className="py-2 px-4">{row.sales}</TableCell>
+                <TableCell className="py-2 px-4">
+                  <div className="w-full bg-gray-200 rounded-full h-2.5">
+                    <div
+                      className="bg-primary h-2.5 rounded-full"
+                      style={{ width: `${row.popularity}%` }}
+                    ></div>
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </Card>
+  );
+};
+
+interface RequestsCardProps {
+  requestsData: any[];
+}
+
+const RequestsCard = ({ requestsData }: RequestsCardProps) => {
+  const t = useTranslations();
+  return (
+    <Card className=" h-full p-4 flex flex-col gap-2">
+      <CardTitle className="flex justify-between items-center">
+        <span className="text-lg text-primary">{t("Providers.requests")}</span>
+        <Link
+          href={`/dashboard/requests`}
+          className="text-sm hover:text-foreground/80"
+        >
+          {t("Providers.view_all")}
+        </Link>
+      </CardTitle>
+      <div className="overflow-auto max-h-[35vh]">
+        <Table className="min-w-full text-sm">
+          <TableHeader>
+            <TableRow className="bg-muted">
+              <TableHead className="py-2 px-4 text-start">
+                {t("Providers.name")}
+              </TableHead>
+              <TableHead className="py-2 px-4 text-start">
+                {t("Providers.request_date")}
+              </TableHead>
+              <TableHead className="py-2 px-4 text-center">
+                {t("Providers.action")}
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {requestsData.slice(0, 5).map((row, index) => (
+              <TableRow key={index} className="hover:bg-secondary">
+                <TableCell className="py-2 px-4">{row.name}</TableCell>
+                <TableCell className="py-2 px-4">
+                  {row.requestDateTime}
+                </TableCell>
+                <TableCell className="py-2 px-4">
+                  <div className="flex gap-2 justify-center">
+                    <button className="bg-green-500 text-white px-2 py-1 rounded hover:bg-green-600 text-xs">
+                      {t("Providers.accept")}
+                    </button>
+                    <button className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600 text-xs">
+                      {t("Providers.reject")}
+                    </button>
+                    <button className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600 text-xs">
+                      {t("Providers.viewDetails")}
+                    </button>
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </Card>
+  );
+};
+
+export default AdminDashboardPage;
--- components/dashboard/cards-carousel.tsx
+++ components/dashboard/cards-carousel.tsx
@@ -10,43 +10,47 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import Image from "next/image";
+import { useLocale } from "next-intl";
 
 export function CardsCarousel() {
   const plugin = React.useRef(
     Autoplay({ delay: 2000, stopOnInteraction: false }),
   );
-
+  const isArabic = useLocale() === "ar";
   return (
-    <div className="h-full w-full rounded-2xl">
-      <Carousel
-        plugins={[plugin.current]}
-        className="w-full h-full  rounded-2xl  "
-        // onMouseEnter={plugin.current.stop}
-        // onMouseLeave={plugin.current.reset}
-      >
-        <div className="  h-full w-full  ">
-          <CarouselContent className="h-full flex  ">
-            {Array.from({ length: 3 }).map((_, index) => (
-              <CarouselItem key={index} className="h-full  ">
-                <div className="relative rounded-2xl overflow-hidden h-full w-full">
-                  <Image
-                    fill
-                    src="/event.jpg"
-                    alt=""
-                    className="object-cover" // you can keep rounded here but not required anymore
-                  />
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-        </div>
+    <Carousel
+      plugins={[plugin.current]}
+      className="w-full h-full  rounded-2xl  "
+      // onMouseEnter={plugin.current.stop}
+      // onMouseLeave={plugin.current.reset}
+    >
+      <div className="  h-full w-full  ">
+        <CarouselContent
+          className={`h-full flex  ${isArabic ? "flex-row-reverse" : ""}`}
+        >
+          {Array.from({ length: 3 }).map((_, index) => (
+            <CarouselItem key={index} className="h-full  ">
+              <div className="relative rounded-2xl overflow-hidden h-full w-full">
+                <Image fill src="/event.jpg" alt="" className="object-cover " />
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </div>
 
-        <div className="rounded-2xl z-50 -mt-10 bg-gradient-to-t from-black/90 to-transparent px-2 flex items-center justify-between text-white relative">
-          <CarouselPrevious className="z-50 mt-2 static " variant="secondary" />
-          <div className="flex-1 text-center">z-50</div>
-          <CarouselNext className="z-50 mt-2 static" variant="secondary" />
-        </div>
-      </Carousel>
-    </div>
+      <div className="rounded-2xl  -mt-10 bg-gradient-to-t from-black/90 to-transparent px-2 flex items-center justify-between text-white relative">
+        <CarouselPrevious
+          className=" mt-2 static"
+          variant="secondary"
+          style={{ order: isArabic ? 2 : 0 }}
+        />
+        <div className="flex-1 text-center order-1">z-50</div>
+        <CarouselNext
+          className=" mt-2 static"
+          variant="secondary"
+          style={{ order: isArabic ? 0 : 2 }}
+        />
+      </div>
+    </Carousel>
   );
 }
