da2925543e09911347c07756aa95dfe5f285892b
Elyas Hamedeh <hamedahty2000@gmail.com>
2025-06-21 16:05:59 +0300

    add criteria styling fix "margin for generalcriteria"

--- app/add-criteria/AddCriterionDialog.tsx
+++ app/add-criteria/AddCriterionDialog.tsx
@@ -2,7 +2,15 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { formSchema, FormValues } from "./schemas";
-import { Form, FormDescription, FormField } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -20,25 +28,20 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import {
-  FormControl,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+
 import { formFieldTypes } from "./FormFeildTypes";
 import { Checkbox } from "@/components/ui/checkbox";
 import api from "@/lib/api";
-import { useState } from "react";
 import { LoadingButton } from "@/components/ui/loading-buuton";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
 import { toast } from "sonner";
 
 interface AddCriterionDialogProps {
   refresh?: () => void;
 }
 export function AddCriterionDialog({ refresh }: AddCriterionDialogProps) {
-  const [isOpen, setIsOpen] = useState(false); // State to control dialog
+  const [isOpen, setIsOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const form = useForm<FormValues>({
@@ -163,124 +166,5 @@ export function AddCriterionDialog({ refresh }: AddCriterionDialogProps) {
         </Form>
       </DialogContent>
     </Dialog>
-  );
-}
-("use client");
-import { useState } from "react";
-import { toast } from "sonner";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import {
-  MultiSelector,
-  MultiSelectorContent,
-  MultiSelectorInput,
-  MultiSelectorItem,
-  MultiSelectorList,
-  MultiSelectorTrigger,
-} from "@/components/ui/multi-select";
-
-const formSchema = z.object({
-  type_name: z.string().min(1),
-  criteria_list: z
-    .array(z.string())
-    .nonempty("Please at least one item")
-    .optional(),
-});
-
-export default function MyForm() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      criteria_list: ["React"],
-    },
-  });
-
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    try {
-      console.log(values);
-      toast(
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
-        </pre>,
-      );
-    } catch (error) {
-      console.error("Form submission error", error);
-      toast.error("Failed to submit the form. Please try again.");
-    }
-  }
-
-  return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="space-y-8 max-w-3xl mx-auto py-10"
-      >
-        <FormField
-          control={form.control}
-          name="type_name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Type</FormLabel>
-              <FormControl>
-                <Input placeholder="eg, collaborative" type="" {...field} />
-              </FormControl>
-
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="criteria_list"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Criteria</FormLabel>
-              <FormControl>
-                <MultiSelector
-                  values={field.value}
-                  onValuesChange={field.onChange}
-                  loop
-                  className="max-w-xs"
-                >
-                  <MultiSelectorTrigger>
-                    <MultiSelectorInput placeholder="Select languages" />
-                  </MultiSelectorTrigger>
-                  <MultiSelectorContent>
-                    <MultiSelectorList>
-                      <MultiSelectorItem value={"React"}>
-                        React
-                      </MultiSelectorItem>
-                      <MultiSelectorItem value={"Vue"}>Vue</MultiSelectorItem>
-                      <MultiSelectorItem value={"Svelte"}>
-                        Svelte
-                      </MultiSelectorItem>
-                    </MultiSelectorList>
-                  </MultiSelectorContent>
-                </MultiSelector>
-              </FormControl>
-              <FormDescription>
-                select criteria to assign it to this type
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
   );
 }
