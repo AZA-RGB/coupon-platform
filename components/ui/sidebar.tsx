bb073d57ec252154a82a9c214bac8588f9242b80
Elyas Hamedeh <hamedahty2000@gmail.com>
2025-07-11 18:26:42 +0300

    Fix mobile sidebar styling and placement
    - Changed the sidebar background to include a top border
    - Set the side to "bottom" for mobile view
    - Added rounded corners
    - Set the sidebar to span the entire width.
    - Removed the font-bold

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -187,13 +187,13 @@ function Sidebar({
           data-sidebar="sidebar"
           data-slot="sidebar"
           data-mobile="true"
-          className="bg-sidebar text-sidebar-foreground w-(--sidebar-width) p-0 [&>button]:hidden"
+          className="bg-sidebar border-t-2 rounded-t-4xl font-bold text-sidebar-foreground w-full p-0 [&>button]:hidden"
           style={
             {
               "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
             } as React.CSSProperties
           }
-          side={side}
+          side="bottom"
         >
           <SheetHeader className="sr-only">
             <SheetTitle>Sidebar</SheetTitle>
