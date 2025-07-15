7137fe7875546c20760943364b8e1bad0cb60778
Elyas Hamedeh <hamedahty2000@gmail.com>
2025-07-16 01:35:35 +0300

    report page ui enhancment

--- components/report-page/BillinInfo.tsx
+++ components/report-page/BillinInfo.tsx
@@ -1,8 +1,16 @@
-import { Button } from "../ui/button";
-import { Card } from "../ui/card";
-import { Table,TableBody, TableCell, TableHead, TableHeader, TableRow } from "../ui/table";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useTranslations, useFormatter, useLocale } from "next-intl";
 
-    const testData = [
+const testData = [
   {
     billID: "#12345",
     coupon: "shawarma coupon",
@@ -95,39 +103,56 @@ import { Table,TableBody, TableCell, TableHead, TableHeader, TableRow } from "..
     customers: ["Youssef Ali", "Mona Hassan"],
   },
 ];
+
 export default function BillingInfo() {
+  const t = useTranslations("BillingInfo");
+  const format = useFormatter();
+  const locale = useLocale();
+  const isRTL = locale === "ar";
 
   return (
-    <Card className="col-span-2 grid grid-rows-6 gap-1 px-3 pt-0 h-[70vh]">
+    <Card
+      className={`md:col-span-2 col-span-1 grid grid-rows-6 gap-1 px-3 pt-0 h-[70vh] ${isRTL ? "text-right" : "text-left"}`}
+    >
       <div className="row-span-1 flex flex-row place-content-between items-center">
-        <div className="text-3xl text-primary">Billing information</div>
-        <Button variant="outline" className=" dark:">
-          Export
-        </Button>
+        <div className="text-3xl text-primary">{t("title")}</div>
+        <Button variant="outline">{t("exportButton")}</Button>
       </div>
       <div className="row-span-5 overflow-auto">
         <Table>
-          <TableHeader className="bg-muted">
+          <TableHeader className="bg-muted text-accent-foreground">
             <TableRow>
-              <TableHead className="w-[100px]">Bill ID</TableHead>
-              <TableHead>Coupon</TableHead>
-              <TableHead>Date</TableHead>
-              <TableHead>Customers</TableHead>
+              <TableHead className="rtl:text-right">
+                {t("tableHeaders.billId")}
+              </TableHead>
+              <TableHead className="rtl:text-right">
+                {t("tableHeaders.coupon")}
+              </TableHead>
+              <TableHead className="rtl:text-right">
+                {t("tableHeaders.date")}
+              </TableHead>
+              <TableHead className="rtl:text-right">
+                {t("tableHeaders.customers")}
+              </TableHead>
+              <TableHead className="rtl:text-right">{t("price")}</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            {testData.map((bill, index) => (
-              <TableRow key={index}>
-                <TableCell className="font-medium">{bill.billID}</TableCell>
-                <TableCell>{bill.coupon}</TableCell>
-                <TableCell>{bill.date}</TableCell>
-                <TableCell>
-                  {bill.customers.map((customer, i) => (
-                    <div key={i}>{customer}</div>
-                  ))}
-                </TableCell>
-              </TableRow>
-            ))}
+            {testData.map((bill, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell className="font-medium">{bill.billID}</TableCell>
+                  <TableCell>{bill.coupon}</TableCell>
+                  <TableCell>{bill.date}</TableCell>
+                  <TableCell>
+                    {bill.customers.map((customer, i) => (
+                      <div key={i}>{customer}</div>
+                    ))}
+                  </TableCell>
+                  <TableCell className="text-right">{bill.price}</TableCell>
+                </TableRow>
+              );
+            })}
           </TableBody>
         </Table>
       </div>
